06cfa0d74ba5e6ebf903e7254d839fa6555f9a07
sanjay5577 <sanjaybms17@gmail.com>
2024-08-10 17:40:34 +0530

    Handled UI Validation

--- multi-form-app/src/LinearStepper.js
+++ multi-form-app/src/LinearStepper.js
@@ -1,5 +1,5 @@
-import React, { useState , memo} from "react";
-import {useEffect} from 'react'
+import React, { useState } from "react";
+
 import {
   Typography,
   TextField,
@@ -12,13 +12,12 @@ import { Box } from "@mui/system";
 
 import{useForm  ,FormProvider  , useFormContext , Controller } from "react-hook-form"
 
-const MemoizedTextField = memo((props) => {
-  return <TextField {...props} />;
-});
+
 
 
 const LinearStepper = () => {
 
+  
   const [activeStep, setActiveStep] = useState(0);
 
 const savedMultiForm = JSON.parse(localStorage.getItem('react-multi-form-data'));
@@ -32,6 +31,29 @@ const savedMultiForm = JSON.parse(localStorage.getItem('react-multi-form-data'))
         state: "",
         zipcode: "",
   })
+  const methods = useForm({
+    defaultValues: formData,
+    shouldUnregister: false, 
+   mode: "all"
+  }); // Initialize form context
+
+  const {
+    trigger,
+  
+  } = methods;
+
+  console.log(formData);
+
+ const handleChange =(e)=>{
+  setFormData(prevData => ({
+    ...prevData,
+    [e.target.name]: e.target.value
+  }));
+  localStorage.setItem("react-multi-form-data", JSON.stringify({
+    ...formData,
+    [e.target.name]: e.target.value
+  }));
+ }
 
 function getSteps() {
   return [
@@ -41,35 +63,81 @@ function getSteps() {
   ];
 }
 
-const handleChange = (e)=>{
-  setFormData( 
-   {...formData,[e.target.name] :e.target.value}
-  )
- }
 
-const PersonalInformation = ()=>{
-  return(
+ const PersonalInformation = () => {
+  const { control , formState: {errors},} = useFormContext(); 
+
+  return( 
     <Box className="content">
-    <TextField
+      <Controller  
+        control={control}
+        name="name"
+        rules ={{required : "Name is required.",
+        validate: (value) =>
+              value.trim() !== "" || "Name cannot be empty.",
+        }}
+        render={({ field }) => (
+          <TextField
             id="name"
             label="Name"
             variant="outlined"
             title="Name"
-            name="name"
             placeholder="Enter Name"
             fullWidth
-            onChange={(e)=> {handleChange(e)}}
+            {...field}  // Spread the field props which includes value and onChange
+            onBlur={(e) => {
+              field.onBlur();  // Call react-hook-form's onBlur
+              handleChange(e)
+            }}
+
+            error ={Boolean(errors.name)}
+            helperText = {errors.name?.message}
           />
+        )}
+      />
+
+      <Controller  
+        control={control}
+        name="email"
+        rules ={{required : "Email is required.",
+        pattern: {
+          value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+          message: "Invalid email address",
+        },
+      }}
+        render={({ field }) => (
           <TextField
             id="email"
             label="Email"
             variant="outlined"
             placeholder="Enter Your Email"
             fullWidth
-            margin="normal"
-            name="email"
-            onChange={handleChange}
+            margin="normal"  
+            {...field}  // Spread the field props
+            onBlur={(e) => {
+              field.onBlur();  // Call react-hook-form's onBlur
+              handleChange(e)
+            }}
+            error ={Boolean(errors.email)}
+            helperText = {errors.email?.message}
           />
+        )}
+      />
+
+      <Controller  
+        control={control}
+        name="phonenumber"
+        rules ={{required : "Phone Number is required.",
+        pattern: {
+          value: /^\d{10}$/, // Pattern for 10-digit phone number
+          message: "Invalid phone number, must be 10 digits",
+        },
+        validate: {
+          notAllZeros: (value) =>
+            value !== "0000000000" || "Phone number cannot be all zeros",
+        },
+        }}
+        render={({ field }) => (
           <TextField
             id="phone-number"
             label="Phone Number"
@@ -77,66 +145,165 @@ const PersonalInformation = ()=>{
             placeholder="Enter Your Phone Number"
             fullWidth
             margin="normal"
-            name="phonenumber"
-            onChange={handleChange}
+            {...field}  // Spread the field props
+            onBlur={(e) => {
+              field.onBlur();  // Call react-hook-form's onBlur
+              handleChange(e)
+            }}
+            error ={Boolean(errors.phonenumber)}
+            helperText = {errors.phonenumber?.message}
           />
+        )}
+      />
     </Box>
   )
 }
 
+
 const AddressInformation = ()=> {
+  const { control , formState: {errors},} = useFormContext(); 
   return(
     <>
-    <TextField
-            id="addressline1"
-            label="Address Line 1"
-            variant="outlined"
-            placeholder="Enter Your Address Line 1"
-            fullWidth
-            margin="normal"
-            name="addressline1"
-            onChange={handleChange}
-          />
+          <Controller  
+        control={control}
+        name="addressline1"
+        rules ={{required : "Address Line 1 is required.",
+        validate: (value) =>
+              value.trim() !== "" || "Address Line 1 cannot be empty.",
+        
+        }}
+        render={({ field }) => (
           <TextField
-            id="addressline2"
-            label="Address Line 2"
-            variant="outlined"
-            placeholder="Enter Your Address Line 2"
-            fullWidth
-            margin="normal"
-            name="addressline2"
-            onChange={handleChange}
+          id="addressline1"
+          label="Address Line 1"
+          variant="outlined"
+          placeholder="Enter Your Address Line 1"
+          fullWidth
+          margin="normal"
+            {...field}  // Spread the field props
+            onBlur={(e) => {
+              field.onBlur();  // Call react-hook-form's onBlur
+              handleChange(e)
+            }}
+            error = {Boolean(errors.addressline1)}
+            helperText = {errors.addressline1?.message}
           />
+        )}
+      />
+       
+       <Controller  
+        control={control}
+        name="addressline2"
+        rules ={{required : "Address Line 2 is required.",
+        validate: (value) =>
+              value.trim() !== "" || "Address Line 2 cannot be empty.",
+        }}
+        render={({ field }) => (
           <TextField
-            id="city"
-            label="City"
-            variant="outlined"
-            placeholder="Enter Your City Name"
-            fullWidth
-            margin="normal"
-            name="city"
-            onChange={handleChange}
+          id="addressline2"
+          label="Address Line 2"
+          variant="outlined"
+          placeholder="Enter Your Address Line 2"
+          fullWidth
+          margin="normal"
+            {...field}  // Spread the field props
+            onBlur={(e) => {
+              field.onBlur();  // Call react-hook-form's onBlur
+              handleChange(e)
+            }}
+          
+          error ={Boolean(errors.addressline2)}
+          helperText = {errors.addressline2?.message}
           />
+        )}
+      />
+
+<Controller  
+        control={control}
+        name="city"
+        rules ={{required : "City is required.",
+        validate: (value) =>
+              value.trim() !== "" || "City cannot be empty.",
+        }}
+        render={({ field }) => (
           <TextField
-            id="state"
-            label="State"
-            variant="outlined"
-            placeholder="Enter Your State Name"
-            fullWidth
-            margin="normal"
-            name="state"
-            onChange={handleChange}
+          id="city"
+          label="City"
+          variant="outlined"
+          placeholder="Enter Your City Name"
+          fullWidth
+          margin="normal"
+            {...field}  // Spread the field props
+            onBlur={(e) => {
+              field.onBlur();  // Call react-hook-form's onBlur
+              handleChange(e)
+            }}
+            error ={Boolean(errors.city)}
+            helperText = {errors.city?.message}
           />
+        )}
+      />
+        
+        <Controller  
+        control={control}
+        name="state"
+        rules ={{required : "State is required.",
+        validate: (value) =>
+              value.trim() !== "" || "State cannot be empty.",
+        }}
+        render={({ field }) => (
           <TextField
-            id="zipcode"
-            label="Zip Code"
-            variant="outlined"
-            placeholder="Enter Your Zip Code"
-            fullWidth
-            margin="normal"
-            name="zipcode"
-            onChange={handleChange}
+          id="state"
+          label="State"
+          variant="outlined"
+          placeholder="Enter Your State Name"
+          fullWidth
+          margin="normal"
+            {...field}  // Spread the field props
+            onBlur={(e) => {
+              field.onBlur();  // Call react-hook-form's onBlur
+              handleChange(e)
+            }}
+            error ={Boolean(errors.state)}
+            helperText = {errors.state?.message}
           />
+        )}
+      />
+
+
+<Controller  
+        control={control}
+        name="zipcode"
+        rules ={{required : "Zip Code is required.",
+        pattern: {
+                value: /^\d{6}$/, // Pattern for 10-digit phone number
+                message: "Invalid Zip Code, must be 6 digits",
+              },
+         validate: {
+                notAllZeros: (value) =>
+                  value !== "000000" || "Zip Code cannot be all zeros",
+         },
+        }}
+        render={({ field }) => (
+          <TextField
+          id="zipcode"
+          label="Zip Code"
+          variant="outlined"
+          placeholder="Enter Your Zip Code"
+          fullWidth
+          margin="normal"
+            {...field}  // Spread the field props
+            onBlur={(e) => {
+              field.onBlur();  // Call react-hook-form's onBlur
+              handleChange(e)
+            }}
+            error ={Boolean(errors.zipcode)}
+            helperText = {errors.zipcode?.message}
+          />
+        )}
+      />
+
+
     </>
   )
 }
@@ -162,7 +329,7 @@ const Confirmation = ()=>{
         Phone Number: 
       </Typography>
       <Typography variant="body1" component="span">
-        {` ${formData.phonennumber}`}
+        {` ${formData.phonenumber}`}
       </Typography>
       <br />
       <Typography variant="h6" component="span">
@@ -234,16 +401,16 @@ function getStepContent(step) {
 
   const steps = getSteps();
 
-  useEffect(()=>{
-    localStorage.setItem("react-multi-form-data" , JSON.stringify(formData))
 
-  } , [formData])
-
-
-
-  const handleNext = () => {
-    setActiveStep(activeStep + 1);
-
+  const handleNext = async() => {
+    const isStepValid = await methods.trigger();
+    if (isStepValid) {
+      setActiveStep((prevStep) => prevStep + 1);
+    }
+    // if(! Boolean(Object.keys(methods.formState.errors).length)){
+    //   setActiveStep(activeStep + 1);
+    // }
+    
    
   };
 
@@ -256,6 +423,9 @@ function getStepContent(step) {
     setActiveStep(activeStep - 1);
   };
 
+  const isStepFailed =()=>{
+    return Boolean(Object.keys(methods.formState.errors).length)
+  }
 
 
   return (
@@ -264,6 +434,10 @@ function getStepContent(step) {
         {steps.map((step, index) => {
           const labelProps = {};
           const stepProps = {};
+
+          if(isStepFailed() && activeStep === index){
+            labelProps.error = true
+          }
 
           return (
             <Step {...stepProps} key={index}>
@@ -280,7 +454,7 @@ function getStepContent(step) {
       ) : (
         <>
 
-           <FormProvider >
+           <FormProvider  {...methods}>
           <form>{getStepContent(activeStep)}</form>
           </FormProvider >
           <Button
